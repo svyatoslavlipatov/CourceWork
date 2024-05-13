c52e65c8692d11e17bdd0f86c914f0e209691ff8
Yawnmain <Yawnishe.671@yandex.ru>
2024-05-13 11:20:53 +0800

    /

--- assets/js/per.js
+++ assets/js/per.js
@@ -1,9 +1,9 @@
-// (function() {
-//     // Создаём таймер
-//     setTimeout(function() {
-//       document.querySelector('body').style.overflow = 'auto';
-//       var el = document.getElementById('preloader');
-//       el.remove();
+(function() {
+    // Создаём таймер
+    setTimeout(function() {
+      document.querySelector('body').style.overflow = 'auto';
+      var el = document.getElementById('preloader');
+      el.remove();
 
-//     }, 5500); // 10000 мсек = 10 сек
-//   })();
+    }, 5500); // 10000 мсек = 10 сек
+  })();
